5bab1aae8abee366cf110ad95266bf4b2fb5d968
Anukrit-Gupta <77784910+Anukrit-Gupta@users.noreply.github.com>
2021-02-12 17:50:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var fixedRect,movingRect;
+var object1,object2;
 
 function setup() {
   createCanvas(1600,800);
@@ -6,8 +7,14 @@ function setup() {
   movingRect = createSprite(400,200,80,30);
   fixedRect.shapeColor = "green";
   movingRect.shapeColor = "green";
-  fixedRect.debug=true;
-  movingRect.debug=true;
+
+  object1 = createSprite(400,200,50,50);
+  object2 = createSprite(600,200,50,50);
+  object1.shapeColor = "green";
+  object2.shapeColor = "green";
+
+  //fixedRect.debug=true;
+  //movingRect.debug=true;
 }
 
 function draw() {
@@ -15,23 +22,18 @@ function draw() {
  
  movingRect.x=mouseX;
  movingRect.y=mouseY;
-  console.log(fixedRect.x);
-  console.log(movingRect.x);
-  
-  if(movingRect.x-fixedRect.x<=fixedRect.width/2 + movingRect.width/2 &&
-     fixedRect.x-movingRect.x<=fixedRect.width/2 + movingRect.width/2 &&
-    fixedRect.y-movingRect.y<=fixedRect.height/2 + movingRect.height/2 &&
-    movingRect.y-fixedRect.y<=fixedRect.height/2 + movingRect.height/2
-  ) {
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
-  }
-  else{ 
-    fixedRect.shapeColor = "green";
-    movingRect.shapeColor = "green";
+
+  if(isTouching(fixedRect,movingRect)) {
+    fixedRect.shapeColor = "blue";
+  movingRect.shapeColor = "blue";
 
   }
-  
-  
-  drawSprites();
+  else {
+    fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+  }
+ drawSprites();
+ 
 }
+
+
